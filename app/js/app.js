e4991b7e676bb5933cf29ee4bcd683ecc38f20fe
Thierry LAU <thierlau@gmail.com>
2013-01-19 22:27:32 +0100

    changed ng-view tag

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
+"use strict";
+
 var angularMovieApp = angular.module('angularMovieApp', []);
+
+angularMovieApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'partials/home.html',
+            controller : 'homeController'
+        })
+        .when('/movies', {
+            templateUrl: 'partials/movies.html',
+            controller : 'moviesController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
