901fe697e491f335a358ffbaf601f4fc8cef8b8a
Thierry LAU <thierlau@gmail.com>
2013-01-19 22:19:05 +0100

    http

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,65 +6,28 @@ angularMovieApp.controller("homeController" ,function ($scope) {
 
 });
 
-angularMovieApp.controller("moviesController" ,function ($scope) {
+angularMovieApp.controller("moviesController" ,function ($scope, $http) {
 
-    $scope.movies = [
-        {
-            id: 1,
-            title : "Avatar",
-            releaseYear : "2010",
-            poster : "img/avatar.jpg",
-            directors : "James Cameron",
-            actors : "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang, Michelle Rodriguez",
-            synopsis : "Sur la lointaine planète de Pandora, Jake Sully, un héros malgré lui, " +
-                "se lance dans une quête de rédemption, de découverte, d'amour inattendu, dont l'issue sera un " +
-                "combat héroïque pour sauver toute une civilisation.",
-            rate : "3"
-        },
-        {
-            id: 2,
-            title : "Seigneur des Anneaux : La Communauté de l'Anneau",
-            releaseYear : "2003",
-            poster : "img/seigneurdesanneaux1.jpg",
-            directors : "Peter Jackson",
-            actors : "Elijah Wood, Sean Astin, Ian McKellen, Sala Baker, Viggo Mortensen",
-            synopsis : "Frodon le Hobbit hérite de l'Anneau Unique, un instrument de pouvoir absolu" +
-                "qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu." +
-                " Commence alors un vaste périple visant à la destruction de l'objet.",
-            rate : "5"
-        },
-        {
-            id: 3,
-            title : "The Grudge",
-            releaseYear : "2004",
-            poster : "img/thegrudge.jpg",
-            directors : "Takashi Shimizu",
-            actors : "Sarah Michelle Gellar, Jason Behr, Clea DuVall, Kadee Strickland, Bill Pullman",
-            synopsis : "Dans ce qui paraît être une paisible maison de Tokyo se cache un épouvantable fléau. " +
-                "Quiconque franchit le seuil de la demeure est aussitôt frappé par une malédiction qui ne tardera " +
-                "pas à le tuer dans un sentiment d'indicible rage...",
-            rate : "4"
-        },
-        {
-            id: 4,
-            title : "Yip Man 2",
-            releaseYear : "2010",
-            poster : "img/yipman.jpg",
-            directors : "Wilson Yip",
-            actors : "Donnie Yen, Sammo Hung Kam-Bo, Simon Yam, Lynn Hung, Xiaoming Huang",
-            synopsis : "Film biographique sur la vie de Ip Man, pionnier du Wing Chun et maitre de Bruce Lee.",
-            rate : "5"
-        }
-    ];
+    $http.get('/server/api/movies').success(function(resp){
+        $scope.movies = resp.movies;
+    });
 
 });
 
-angularMovieApp.controller("movieFormController" ,function ($scope) {
+angularMovieApp.controller("movieFormController" ,function ($scope, $http) {
 
     $scope.addMovie = function(movie){
-        var newMovie = {};
-        angular.copy(movie, newMovie);
-        $scope.movies.push(newMovie);
+
+        $http.post('/server/api/movies', movie)
+            .success(function(){
+                var newMovie = {};
+                angular.copy(movie, newMovie);
+                $scope.movies.push(newMovie);
+            })
+            .error(function(resp){
+                console.log(resp);
+            });
+
     };
 
 });
