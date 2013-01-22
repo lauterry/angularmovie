5f6de167ab3b67995d25ff3a0501663cfe23a394
Thierry LAU <thierlau@gmail.com>
2013-01-23 00:36:45 +0100

    solution : filters

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,7 +1,27 @@
-/**
- * Created with IntelliJ IDEA.
- * User: thierlau
- * Date: 22/01/13
- * Time: 23:58
- * To change this template use File | Settings | File Templates.
- */
+"use strict";
+
+angularMovieApp.filter('stars', function () {
+
+    var STARS = {
+        1: '\u2605',
+        2: '\u2605\u2605',
+        3: '\u2605\u2605\u2605',
+        4: '\u2605\u2605\u2605\u2605',
+        5: '\u2605\u2605\u2605\u2605\u2605'
+    };
+
+    return function(startCount) {
+        return STARS[startCount];
+    };
+});
+
+
+angularMovieApp.filter('poster', function () {
+    return function(posterUrl) {
+        if(!posterUrl){
+            return "img/no-poster.jpg";
+        } else {
+            return posterUrl;
+        }
+    };
+});
