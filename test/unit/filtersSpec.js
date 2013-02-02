833cd8403d8a76089cd5ebfc36a590f2e4cda585
Thierry LAU <thierlau@gmail.com>
2013-02-02 14:31:39 +0100

    add filter test

---
+++ test/unit/filtersSpec.js
@@ -0,0 +1,15 @@
+describe('filters', function() {
+
+    beforeEach(module('angularMovieApp'));
+
+    it('should show proper number of dollars', inject(function(starsFilter) {
+        expect(starsFilter(1)).toBe('\u2605');
+        expect(starsFilter(3)).toBe('\u2605\u2605\u2605');
+    }));
+
+    it('should show default picture if none is provided', inject(function(posterFilter) {
+        expect(posterFilter('')).toBe('img/no-poster.jpg');
+        expect(posterFilter('img/picture.jpg')).toBe('img/picture.jpg');
+
+    }));
+});
