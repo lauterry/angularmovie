def8e7eabe5566addc6ed0d916193d1fcaa43450
Thierry LAU <thierlau@gmail.com>
2013-02-12 08:25:13 +0100

    refactor addMovie method and refactor route injection

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -37,11 +37,10 @@ angularMovieApp.controller("movieFormController" ,function ($scope, Movie) {
     $scope.addMovie = function(movie){
         Movie.create(movie)
             .success(function(){
-                var newMovie = {};
-                angular.copy(movie, newMovie);
-                $scope.movies.push(newMovie);
+                $scope.movies.push(movie);
                 $scope.movie = {};
                 $scope.showAlert = false;
+                $('#movie-form-modal').modal('hide');
             })
             .error(function(resp, statusCode){
                 // Affichage d'un message d'erreur
