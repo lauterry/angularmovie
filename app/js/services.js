b42ebd604bd12572871c9a85af40d90b9f4c2ea0
Thierry LAU <thierlau@gmail.com>
2013-02-17 22:47:58 +0100

    solve cherry pick

--- app/js/services.js
+++ app/js/services.js
@@ -11,6 +11,21 @@ angularMovieApp.service("Movie", function ($http) {
 
         create : function(movie) {
             return  $http.post(API_URI, movie);
+<<<<<<< HEAD
+=======
+        },
+
+        remove : function(id) {
+            return $http.delete(API_URI + '/' + id);
+        },
+
+        fetchOne : function(id) {
+            return $http.get(API_URI + '/' + id);
+        },
+
+        update : function(movie) {
+             return $http.put('/server/api/movies', movie);
+>>>>>>> ff2f135... update and fetchOne service
         }
 
     };
