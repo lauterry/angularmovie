8fb6a97283274ac659814f600dc52d7c2e6dd539
Thierry LAU <thierlau@gmail.com>
2013-04-06 19:50:41 +0200

    add editable directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -39,3 +39,53 @@ angularMovieApp.directive('alertMessage', function(){
         }
     };
 });
+
+
+
+/**
+ * inspired by http://www.adobe.com/devnet/html5/articles/angularjs-directives-and-the-computer-science-of-javascript.html
+ * by Burke Holland
+ */
+angularMovieApp.directive('editable', function(){
+
+    return {
+        restrict : 'E',
+        transclude : true,
+        replace : true,
+        template:   '<span>' +
+                        '<label ng-click="edit()" ng-hide="editMode" ng-transclude></label> ' +
+                        '<input type="text" ng-show="editMode" >' +
+                    '</span>',
+        scope : {}, // specify an isolate scope for each editable directive
+        link : function(scope, element){
+
+            // select the needed DOM element from the template
+            var label = element.find('label');
+            var input = element.find('input');
+
+            // default, editMode is unactive
+            scope.editMode = false;
+
+            // attach a blur event to the input
+            input.bind('blur', function(){
+                // here we are outside the angularjs databinding system.
+                // we need to call scope.$apply() to notify angularjs
+                // that the model has changed (here's editMode)
+                scope.$apply(function(){
+                    scope.editMode = false;
+                    label.text(input.val());
+                });
+
+            });
+
+            // called when the label is clicked
+            scope.edit = function() {
+                scope.editMode = true;
+                input.val(label.text());
+            };
+
+        }
+
+    };
+
+});
