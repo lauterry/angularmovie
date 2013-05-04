f04187d39bf9cbd822165857b1e6f23f78f4db7b
Thierry LAU <thierlau@gmail.com>
2013-05-04 11:33:22 +0200

    Move code form controller to HTML5 for switch view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,22 +8,6 @@ angularMovieApp.controller("homeController" ,function ($scope) {
 
 angularMovieApp.controller("moviesController" ,function ($scope, Movie) {
 
-    // display mode by default
-    $scope.tableView = false;
-    // icon by mode by default
-    $scope.tableViewIcon = 'icon-th-list icon-white';
-
-    // function called when changing view mode
-    $scope.toogleView = function() {
-        $scope.tableView = !$scope.tableView;
-
-        if($scope.tableView === false){
-            $scope.tableViewIcon = 'icon-th-list icon-white';
-        } else {
-            $scope.tableViewIcon = 'icon-th icon-white';
-        }
-    };
-
     Movie.fetch().success(function(resp){
         $scope.movies = resp.movies;
     });
