b0930dd108b603c046e203f2e11fc1d675cea345
Thierry LAU <thierlau@gmail.com>
2013-05-20 19:22:02 +0200

    update model on server add angular FR locale

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,9 @@ var movies = [
         synopsis : "Sur la lointaine planète de Pandora, Jake Sully, un héros malgré lui, " +
             "se lance dans une quête de rédemption, de découverte, d'amour inattendu, dont l'issue sera un " +
             "combat héroïque pour sauver toute une civilisation.",
-        rate : 3
+        rate : 3,
+        lastViewDate : new Date(2013, 3, 1, 12, 4, 50),
+        price : 25.46
     },
     {
         id: 2,
@@ -23,7 +25,9 @@ var movies = [
         synopsis : "Frodon le Hobbit hérite de l'Anneau Unique, un instrument de pouvoir absolu" +
             "qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu." +
             " Commence alors un vaste périple visant à la destruction de l'objet.",
-        rate : 5
+        rate : 5,
+        lastViewDate : new Date(2012, 5, 13, 9, 30, 45),
+        price : 34.76
     },
     {
         id: 3,
@@ -35,7 +39,9 @@ var movies = [
         synopsis : "Dans ce qui paraît être une paisible maison de Tokyo se cache un épouvantable fléau. " +
             "Quiconque franchit le seuil de la demeure est aussitôt frappé par une malédiction qui ne tardera " +
             "pas à le tuer dans un sentiment d'indicible rage...",
-        rate : 4
+        rate : 4,
+        lastViewDate : new Date(2006, 11, 28, 6, 3, 5),
+        price : 15.23
     },
     {
         id: 4,
@@ -45,7 +51,9 @@ var movies = [
         directors : "Wilson Yip",
         actors : "Donnie Yen, Sammo Hung Kam-Bo, Simon Yam, Lynn Hung, Xiaoming Huang",
         synopsis : "Film biographique sur la vie de Ip Man, pionnier du Wing Chun et maitre de Bruce Lee.",
-        rate : 5
+        rate : 5,
+        lastViewDate : new Date(2011, 2, 24, 1, 2, 4),
+        price : 23.56
     },
     {
         id: 5,
@@ -57,7 +65,9 @@ var movies = [
         synopsis : "Alors qu'ils suivent des pompiers lors d'une intervention, une reporter et son caméraman " +
             "restent coincés dans un immeuble placé sous quarantaine. A l'intérieur, ils font face à d'étranges " +
             "créatures...",
-        rate : 3
+        rate : 3,
+        lastViewDate : new Date(2009, 7, 30, 20, 30, 32),
+        price : 12.05
     },
     {
         id: 6,
@@ -69,7 +79,9 @@ var movies = [
         synopsis : "Un virus mortel s'est propagé dans un laboratoire souterrain et a mis fin à toute vie humaine. " +
             "Alice et Matt doivent suivre un groupe d'intervention militaire, chargé de neutraliser le super-ordinateur " +
             "que l'on tient pour responsable du désastre.",
-        rate : 4
+        rate : 4,
+        lastViewDate : new Date(2005, 6, 3, 8, 12, 34 ),
+        price : 45.23
     },
     {
         id: 7,
@@ -81,7 +93,9 @@ var movies = [
         synopsis : "Frodon Sacquet, le Hobbit, doit braver de terribles dangers pour tenter de détruire " +
             "l'Anneau Unique, convoité par Sauron, le Seigneur des ténèbres. De leur côté, Gimli, Legolas et Aragorn " +
             "doivent sauver Pippin et Merry, capturés par les Orques...",
-        rate : 3
+        rate : 3,
+        lastViewDate : new Date(2005, 11, 12, 15, 34, 34),
+        price : 14.23
     },
     {
         id: 8,
@@ -93,7 +107,9 @@ var movies = [
         synopsis : "Tandis que les ténèbres se répandent sur la Terre du Milieu, Aragorn se révèle être l'héritier " +
             "caché des rois antiques. Quant à Frodon, toujours tenté par l'Anneau, il voyage à travers les contrées " +
             "ennemies, se reposant sur Sam et Gollum...",
-        rate : 4
+        rate : 4,
+        lastViewDate : new Date(2004, 6, 17, 15, 23, 34),
+        price : 24.56
     },
     {
         id: 9,
@@ -105,7 +121,9 @@ var movies = [
         synopsis : "Sing, un prétendu gangster, doit surmonter son incapacité à manier le sabre et démontrer " +
             "qu'il a toutes les qualités requises pour appartenir au prestigieux gang de Axe. Au même moment, " +
             "ce gang veut régner en maître sur une rue sacrée...",
-        rate : 5
+        rate : 5,
+        lastViewDate : new Date(2009, 10, 20, 13, 34, 23),
+        price : 10.50
     }
 ];
 
