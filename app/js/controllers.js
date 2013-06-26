6312a94ef9d4e84186681fda9696c44556b1d35e
Thierry LAU <thierlau@gmail.com>
2013-06-26 06:53:58 +0200

    fix fetch movies

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,7 +9,7 @@ angularMovieApp.controller("homeController" ,function ($scope) {
 angularMovieApp.controller("moviesController" ,function ($scope, Movie) {
 
     Movie.fetch().success(function(resp){
-        $scope.movies = resp.movies;
+        $scope.movies = resp;
     });
 
     $scope.deleteMovie = function(index){
