fbc2a16eaeec974f11043611ed141c66bb161697
Thierry LAU <thierlau@gmail.com>
2013-06-27 09:42:48 +0200

    update test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -6,12 +6,12 @@ describe('angularMovieApp', function () {
 
         it('should navigate to home', function () {
             browser().navigateTo('/index.html#/home');
-            expect(element('.container').text()).toContain('Bienvenue sur AngularMovie');
+            expect(element('[ng-view]').text()).toContain('Bienvenue sur AngularMovie');
         });
 
         it('should navigate to how it works', function () {
             browser().navigateTo('/index.html#/movies');
-            expect(element('.container').text()).toContain('Ma vidéothèque');
+            expect(element('[ng-view]').text()).toContain('Ma vidéothèque');
         });
 
     });
@@ -36,14 +36,33 @@ describe('angularMovieApp', function () {
             input('search').enter('Seigneur');
             var movieElements = repeater('.thumbnails > li', 'Movie list');
             expect(movieElements.count()).toEqual(3);
+
+            input('search').enter('2010');
+            expect(movieElements.count()).toEqual(2);
+
+            input('search').enter('XXX');
+            expect(movieElements.count()).toEqual(0);
         });
 
         it('should be able to sort by movie title', function() {
-            element(':button.btn-info').click();
+            element('#orderTitleButton').click();
             var movieElements = repeater('.thumbnails > li', 'Movie list');
-            expect(movieElements.column('movie.title')).toEqual(["[REC]","AVATAR","CRAZY KUNG FU","RESIDENT EVIL",
-                "SEIGNEUR DES ANNEAUX : LA COMMUNAUTÉ DE L'ANNEAU","SEIGNEUR DES ANNEAUX : LE RETOUR DU ROI",
-                "SEIGNEUR DES ANNEAUX : LES DEUX TOURS","THE GRUDGE","YIP MAN 2"]);
+            expect(movieElements.column('movie.title')).toEqual(["Yip Man 2","The Grudge",
+                "Seigneur des Anneaux : les deux Tours","Seigneur des Anneaux : le retour du Roi",
+                "Seigneur des Anneaux : La Communauté de l'Anneau","Resident Evil","Crazy Kung Fu","Avatar","[REC]"]);
+            lement('#orderTitleButton').click();
+            expect(movieElements.column('movie.title')).toEqual(["[REC]","Avatar","Crazy Kung Fu","Resident Evil",
+                "Seigneur des Anneaux : La Communauté de l'Anneau","Seigneur des Anneaux : le retour du Roi",
+                "Seigneur des Anneaux : les deux Tours","The Grudge","Yip Man 2"]);
+        });
+
+
+        it('should be able to sort by movie releaseYear', function() {
+            element('#orderYearButton').click();
+            var movieElements = repeater('.thumbnails > li', 'Movie list');
+            expect(movieElements.column('movie.releaseYear')).toEqual(["2010", "2010", "2008", "2005", "2004", "2003", "2002", "2002", "2001"]);
+            element('#orderYearButton').click();
+            expect(movieElements.column('movie.releaseYear')).toEqual(["2001", "2002", "2002", "2003", "2004", "2005", "2008", "2010", "2010"]);
         });
 
     });
--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -4,7 +4,7 @@ describe('filters', function() {
 
     beforeEach(module('angularMovieApp'));
 
-    it('should show proper number of dollars', inject(function(starsFilter) {
+    it('should show proper number of stars', inject(function(starsFilter) {
         expect(starsFilter(1)).toBe('\u2605');
         expect(starsFilter(3)).toBe('\u2605\u2605\u2605');
     }));
