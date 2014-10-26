b490c76042d679cee45fa88be8553fa57d6ab966
LAU Thierry <thierlau@gmail.com>
2014-10-26 10:53:31 +0100

    refine back end
    Conflicts:
    	routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -1,147 +1,12 @@
 "use strict";
 
-var MOVIES = [
-    {
-        id: 1,
-        title : "Avatar",
-        category : 'Sciences Fictions',
-        releaseYear : "2010",
-        poster : "img/avatar.jpg",
-        directors : "James Cameron",
-        actors : "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang, Michelle Rodriguez",
-        synopsis : "Sur la lointaine planète de Pandora, Jake Sully, un héros malgré lui, " +
-            "se lance dans une quête de rédemption, de découverte, d'amour inattendu, dont l'issue sera un " +
-            "combat héroïque pour sauver toute une civilisation.",
-        rate : 3,
-        lastViewDate : new Date(2013, 3, 1, 12, 4, 50),
-        price : 25.46
-    },
-    {
-        id: 2,
-        title : "Seigneur des Anneaux : La Communauté de l'Anneau",
-        category : 'Sciences Fictions',
-        releaseYear : "2001",
-        poster : "img/seigneurdesanneaux1.jpg",
-        directors : "Peter Jackson",
-        actors : "Elijah Wood, Sean Astin, Ian McKellen, Sala Baker, Viggo Mortensen",
-        synopsis : "Frodon le Hobbit hérite de l'Anneau Unique, un instrument de pouvoir absolu" +
-            "qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu." +
-            " Commence alors un vaste périple visant à la destruction de l'objet.",
-        rate : 5,
-        lastViewDate : new Date(2012, 5, 13, 9, 30, 45),
-        price : 34.76
-    },
-    {
-        id: 3,
-        title : "The Grudge",
-        category : 'Horreur',
-        releaseYear : "2004",
-        poster : "img/thegrudge.jpg",
-        directors : "Takashi Shimizu",
-        actors : "Sarah Michelle Gellar, Jason Behr, Clea DuVall, Kadee Strickland, Bill Pullman",
-        synopsis : "Dans ce qui paraît être une paisible maison de Tokyo se cache un épouvantable fléau. " +
-            "Quiconque franchit le seuil de la demeure est aussitôt frappé par une malédiction qui ne tardera " +
-            "pas à le tuer dans un sentiment d'indicible rage...",
-        rate : 4,
-        lastViewDate : new Date(2006, 11, 28, 6, 3, 5),
-        price : 15.23
-    },
-    {
-        id: 4,
-        title : "Yip Man 2",
-        category : 'Art Martial',
-        releaseYear : "2010",
-        poster : "img/yipman.jpg",
-        directors : "Wilson Yip",
-        actors : "Donnie Yen, Sammo Hung Kam-Bo, Simon Yam, Lynn Hung, Xiaoming Huang",
-        synopsis : "Film biographique sur la vie de Ip Man, pionnier du Wing Chun et maitre de Bruce Lee.",
-        rate : 5,
-        lastViewDate : new Date(2011, 2, 24, 1, 2, 4),
-        price : 23.56
-    },
-    {
-        id: 5,
-        title : "[REC]",
-        category : 'Horreur',
-        releaseYear : "2008",
-        poster : "img/rec.jpg",
-        directors : "Paco Plaza, Jaume Balagueró",
-        actors : "Manuela Velasco, Ferran Terraza, Jorge Yamam, Carlos Lasarte, Pablo Rosso",
-        synopsis : "Alors qu'ils suivent des pompiers lors d'une intervention, une reporter et son caméraman " +
-            "restent coincés dans un immeuble placé sous quarantaine. A l'intérieur, ils font face à d'étranges " +
-            "créatures...",
-        rate : 3,
-        lastViewDate : new Date(2009, 7, 30, 20, 30, 32),
-        price : 12.05
-    },
-    {
-        id: 6,
-        title : "Resident Evil",
-        category : 'Horreur',
-        releaseYear : "2002",
-        poster : "img/residentevil.jpg",
-        directors : "Paul W.S. Anderson",
-        actors : "Milla Jovovich, Eric Mabius, Michelle Rodriguez, James Purefoy, Liz May Brice",
-        synopsis : "Un virus mortel s'est propagé dans un laboratoire souterrain et a mis fin à toute vie humaine. " +
-            "Alice et Matt doivent suivre un groupe d'intervention militaire, chargé de neutraliser le super-ordinateur " +
-            "que l'on tient pour responsable du désastre.",
-        rate : 4,
-        lastViewDate : new Date(2005, 6, 3, 8, 12, 34 ),
-        price : 45.23
-    },
-    {
-        id: 7,
-        title : "Seigneur des Anneaux : les deux Tours",
-        category : 'Sciences Fictions',
-        releaseYear : "2002",
-        poster : "img/seigneurdesanneaux2.jpg",
-        directors : "Peter Jackson",
-        actors : "Elijah Wood, Sean Astin, Ian McKellen, Sala Baker, Viggo Mortensen",
-        synopsis : "Frodon Sacquet, le Hobbit, doit braver de terribles dangers pour tenter de détruire " +
-            "l'Anneau Unique, convoité par Sauron, le Seigneur des ténèbres. De leur côté, Gimli, Legolas et Aragorn " +
-            "doivent sauver Pippin et Merry, capturés par les Orques...",
-        rate : 3,
-        lastViewDate : new Date(2005, 11, 12, 15, 34, 34),
-        price : 14.23
-    },
-    {
-        id: 8,
-        title : "Seigneur des Anneaux : le retour du Roi",
-        category : 'Sciences Fictions',
-        releaseYear : "2003",
-        poster : "img/seigneurdesanneaux3.jpg",
-        directors : "Peter Jackson",
-        actors : "Elijah Wood, Sean Astin, Ian McKellen, Sala Baker, Viggo Mortensen",
-        synopsis : "Tandis que les ténèbres se répandent sur la Terre du Milieu, Aragorn se révèle être l'héritier " +
-            "caché des rois antiques. Quant à Frodon, toujours tenté par l'Anneau, il voyage à travers les contrées " +
-            "ennemies, se reposant sur Sam et Gollum...",
-        rate : 4,
-        lastViewDate : new Date(2004, 6, 17, 15, 23, 34),
-        price : 24.56
-    },
-    {
-        id: 9,
-        title : "Crazy Kung Fu",
-        category : 'Art Martial',
-        releaseYear : "2005",
-        poster : "img/crazykungfu.jpg",
-        directors : "Stephen Chow",
-        actors : "Stephen Chow, Wah Yuen, Leung Siu Lung, Dong Zhi Hua, Chiu Chi Ling",
-        synopsis : "Sing, un prétendu gangster, doit surmonter son incapacité à manier le sabre et démontrer " +
-            "qu'il a toutes les qualités requises pour appartenir au prestigieux gang de Axe. Au même moment, " +
-            "ce gang veut régner en maître sur une rue sacrée...",
-        rate : 5,
-        lastViewDate : new Date(2009, 10, 20, 13, 34, 23),
-        price : 10.50
-    }
-];
+
+var _ = require('lodash'),
+	MOVIES = require('./data/movies').movies;
 
 /**
  * Fetch all movies
  * If category query is provided, fetch movies filtered by category
- * @param req
- * @param res
- * @returns {*}
  */
 exports.fetchMovies = function (req, res) {
     var movies = [];
@@ -152,103 +17,97 @@ exports.fetchMovies = function (req, res) {
     } else {
         movies = MOVIES;
     }
-    return res.json(200, {movies : movies});
+    return res.status(200).json(movies);
 
 };
 
 
 /**
  * Fetch a movie by id
- * @param req
- * @param res
- * @returns {*}
  */
 exports.fetchMovie = function (req, res){
-    var id = req.params.id;
+    var id = req.params.id,
 
-    for(var i = 0; i < MOVIES.length; i++){
-        if(MOVIES[i].id == id){
-            return res.json(200, MOVIES[i]);
-        }
-    }
+	movie = _.find(MOVIES, function (movie) {
+		return movie.id == id;
+	});
 
-    return res.json(404);
+	if (movie) {
+		return res.status(200).json(movie);
+	} else {
+		return res.status(404).end();
+	}
 };
 
 /**
  * Fetch actors of a movie
- * @param req
- * @param res
  */
 exports.fetchActorsOfMovie = function(req, res){
-    var id = req.params.id;
+    var id = req.params.id,
 
-    for(var i = 0; i < MOVIES.length; i++){
-        if(MOVIES[i].id == id){
-            return res.json(200, MOVIES[i].actors);
-        }
-    }
-    return res.json(404);
+	movie = _.find(MOVIES, function (movie) {
+		return movie.id == id;
+	});
+
+	if (movie.length !== 0) {
+		return res.status(200).json(movie.actors);
+	} else {
+		return res.status(404).end();
+	}
 }
 
 /**
  * Create a movie
- * @param req
- * @param res
- * @returns {*}
  */
 exports.addMovie = function (req, res) {
-    var movie = req.body;
+    var movieToAdd = req.body;
 
-    for(var idx in MOVIES){
-        if(MOVIES[idx].title === movie.title){
-            return res.json(500, { error: 'Le film ' + movie.title + ' a déjà été ajouté.' });
-        }
-    }
+	var existingMovie = _.find(MOVIES, function (movie) {
+		return movieToAdd.title == movie.title;
+	});
 
-    MOVIES.push(movie);
-    return res.json(201);
-
+	if (existingMovie) {
+		return res.status(500).json({ error: 'Le film ' + existingMovie.title + ' a déjà été ajouté.' });
+	} else {
+		ID ++;
+		movieToAdd.id = ID;
+		MOVIES.push(movieToAdd);
+		return res.status(201).json(movieToAdd);
+	}
 };
 
 
 /**
  * Update a movie
- * @param req
- * @param res
- * @returns {*}
  */
 exports.updateMovie = function(req, res) {
-    var movie = req.body;
-    var id = movie.id;
+    var movietoUpdate = req.body;
 
-    for(var i = 0; i < MOVIES.length; i++){
-        if(MOVIES[i].id === id){
-            MOVIES.splice(i, 1);
-            MOVIES.push(movie);
-            return res.json(200);
-        }
-    }
+	_.forEach(MOVIES, function (movie, index) {
+		if (movie.id == movietoUpdate.id) {
+			MOVIES[index] = movietoUpdate;
+			return res.status(200).end();
+		}
+	});
 
-    return res.json(304);
+	return res.status(304).end();
 };
 
 
 /**
  * Delete a movie
- * @param req
- * @param res
- * @returns {*}
  */
 exports.deleteMovie = function (req, res) {
-    var id = req.params.id;
+    var id = req.params.id,
 
-    for(var i = 0; i < MOVIES.length; i++){
-        if(MOVIES[i].id == id){
-            MOVIES.splice(i, 1);
-            return res.json(200);
-        }
-    }
+	removedMovies = _.remove(MOVIES, function (movie) {
+		return movie.id == id;
+	});
 
-    return res.json(304);
+	if (_.isEmpty(removedMovies)) {
+		return res.status(304).end();
+	} else {
+		return res.status(200).end();
+	}
+
 };
